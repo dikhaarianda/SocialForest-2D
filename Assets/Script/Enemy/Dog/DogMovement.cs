44697e4fc95fda74da070a3b692c99e70b4c5946
dikhaarianda <dikhaarianda@gmail.com>
2023-05-28 02:34:38 +0700

    add: enemy dog movement

--- Assets/Script/Enemy/Dog/DogMovement.cs
+++ Assets/Script/Enemy/Dog/DogMovement.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class DogMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject[] Points;
+    [SerializeField] private float speed;
+    private Animator animator;
+    private int PointIndex = 0;
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Vector2.Distance(Points[PointIndex].transform.position, transform.position) < .1f)
+        {
+            PointIndex++;
+            if (PointIndex >= Points.Length)
+            {
+                PointIndex = 0;
+            }
+        }
+        transform.position = Vector2.MoveTowards(transform.position, Points[PointIndex].transform.position, Time.deltaTime * speed);
+
+        if (PointIndex > 0)
+        {
+            transform.eulerAngles = Vector2.zero;
+            animator.SetTrigger("isWalk");
+        }
+        else
+        {
+            transform.eulerAngles = Vector2.up * 180;
+            animator.SetTrigger("isWalk");
+        }
     }
 }
